1b20b9eaf9322089ada1225847178a6efb522de4
Thi Nguyen <ngthi.nguyen21@gmail.com>
2023-07-31 16:45:06 -0600

    refactor Nav Drawer and code cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,74 +1,36 @@
 import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import CssBaseline from '@mui/material/CssBaseline';
-import Divider from '@mui/material/Divider';
-import Drawer from '@mui/material/Drawer';
-import IconButton from '@mui/material/IconButton';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemText from '@mui/material/ListItemText';
+import {
+  AppBar, Box, Button, CssBaseline, IconButton, Toolbar, Typography
+} from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
+
 import { ThemeProvider } from '@mui/material/styles';
 import { Link, Outlet } from 'react-router-dom';
-
+import { NavDrawer } from './components/Navigation';
 import theme from './Theme';
 
-interface Props {
-  /**
-   * Injected by the documentation to work in an iframe.
-   * You won't need it on your project.
-   */
-  window?: () => Window;
-}
-
-const drawerWidth = 240;
 type PagePaths = { [key: string]: string; };
 const pages: PagePaths = {
-  'Coding' : '/coding',
-  'Blog' : '/blog',
+  'Coding': '/coding',
+  'Blog': '/blog',
   'Resume': '/resume',
   'Contact': '/contact'
 };
 const navItems = Object.keys(pages);
+const brand = 'THI NGUYEN';
 
-export default function App (props: Props) {
-  const { window } = props;
+export default function App() {
   const [mobileOpen, setMobileOpen] = React.useState(false);
 
   const handleDrawerToggle = () => {
     setMobileOpen((prevState) => !prevState);
   };
 
-  const drawer = (
-    <Box onClick={handleDrawerToggle} sx={{ textAlign: 'center' }}>
-      <Typography variant="h6" sx={{ my: 2 }}>
-        THI NGUYEN
-      </Typography>
-      <Divider />
-      <List>
-        {navItems.map((item) => (
-          <ListItem key={item} disablePadding component={Link} to={pages[item]}>
-            <ListItemButton sx={{ textAlign: 'center' }}>
-            <ListItemText primary={item} /> 
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
-    </Box>
-  );
-
-  const container = window !== undefined ? () => window().document.body : undefined;
-
   return (
     <ThemeProvider theme={theme}>
       <Box sx={{ display: 'flex' }}>
         <CssBaseline />
-        <AppBar component="nav">
+        <AppBar component="nav" color="primary">
           <Toolbar>
             <IconButton
               color="inherit"
@@ -82,16 +44,16 @@ export default function App (props: Props) {
             <Typography
               variant="h6"
               component="div"
-              sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
+              sx={{ flexGrow: 1, display: { xs: 'block', sm: 'block' }}}
             >
-              THI NGUYEN
+              {brand}
             </Typography>
-            
+
             <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
               {navItems.map((item) => (
-                <Link to={pages[item]}> 
-                  <Button key={item} sx={{ color: '#fff' }}>
-                    {item} 
+                <Link to={pages[item]}>
+                  <Button key={item} color='secondary'>
+                    {item}
                   </Button>
                 </Link>
               ))}
@@ -99,21 +61,9 @@ export default function App (props: Props) {
           </Toolbar>
         </AppBar>
         <Box component="nav">
-          <Drawer
-            container={container}
-            variant="temporary"
-            open={mobileOpen}
-            onClose={handleDrawerToggle}
-            ModalProps={{
-              keepMounted: true, // Better open performance on mobile.
-            }}
-            sx={{
-              display: { xs: 'block', sm: 'none' },
-              '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
-            }}
-          >
-            {drawer}
-          </Drawer>
+          <NavDrawer 
+              header={brand} handleDrawerToggle={handleDrawerToggle} 
+              navItems={navItems} path={pages} open={mobileOpen} width={240} />
         </Box>
         <Box component="main" sx={{ p: 3 }}>
           <Toolbar />
---
+++ src/components/Navigation.tsx
@@ -0,0 +1,45 @@
+import { Box, Divider, Drawer, Link as MUILink, LinkProps, 
+    List, ListItem, ListItemButton, ListItemText, Typography } from '@mui/material';
+import { Link } from 'react-router-dom';
+
+export const NavLink: React.FC<LinkProps<typeof Link>> = (ps) => <MUILink component={Link} {...ps}> {ps.children} </MUILink>;
+
+interface DrawerProps {
+    header?: string;
+    open?: boolean;
+	handleDrawerToggle: () => void;
+    navItems: string[];
+    path: { [key: string]: string; };
+    width?: number;
+
+}
+export const NavDrawer = (ps: DrawerProps) => (
+    <Drawer
+        variant="temporary"
+        open={ps.open || false}
+        onClose={ps.handleDrawerToggle}
+        ModalProps={{
+            keepMounted: true, // Better open performance on mobile.
+        }}
+        sx={{
+            display: { xs: 'block', sm: 'none' },
+            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: ps.width ?? 240 },
+        }}>
+        <Box onClick={ps.handleDrawerToggle} sx={{ textAlign: 'center' }}>
+            {ps.header ? 
+            <><Typography variant="h6" sx={{ my: 2 }}>{ps.header}</Typography>
+            <Divider /></> 
+            : <></> }
+            
+            <List>
+                {ps.navItems.map((item) => (
+                <ListItem key={item} disablePadding component={Link} to={ps.path[item]}>
+                    <ListItemButton sx={{ textAlign: 'center' }}>
+                    <ListItemText primary={item} />
+                    </ListItemButton>
+                </ListItem>
+                ))}
+            </List>
+        </Box>
+    </Drawer>
+);
