e84cbb2afac4022ab42293180c76c6a420db7db7
Thi Nguyen <ngthi.nguyen21@gmail.com>
2023-08-01 13:58:23 -0600

    Add Home as Index Route

--- src/index.tsx
+++ src/index.tsx
@@ -1,31 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, createRoutesFromElements, RouterProvider, Route } from "react-router-dom";
-// import Home from './pages/Home';
+import Home from './pages/Home';
 import ErrorPage from './pages/ErrorPage';
 import App from './App';
 
 import './index.css';
 
-interface props {page?: string}
-const Root = (ps: props ) => (<div>HI THERE! We are in {ps.page}</div>);
+interface props { page?: string }
+const Root = (ps: props) => (<div>HI THERE! We are in {ps.page}</div>);
 
 const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<App />} errorElement={<ErrorPage />}>
-      <Route path="/coding" element={<Root page="Coding" />} />
-      <Route path="/blog" element={<Root page="Blog" />} />
-      <Route path="/resume" element={<Root page="Resume" />} />
-      <Route path="/work-with-me" element={<Root page="Work with Me" />} />
-    </Route>
-  )
+    createRoutesFromElements(
+        <Route path="/" element={<App />} errorElement={<ErrorPage />}>
+            <Route errorElement={<ErrorPage />}>
+                <Route index element={<Home />} />
+                <Route path="/coding/" element={<Root page="Coding" />} />
+                <Route path="/blog" element={<Root page="Blog" />} />
+                <Route path="/resume" element={<Root page="Resume" />} />
+                <Route path="/work-with-me" element={<Root page="Work with Me" />} />
+            </Route>
+        </Route>
+    )
 );
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+    document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>
 );
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,8 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
 
 export const Home = () => (
 	<div>
-		<p>Welcome to Thi Nguyen home page</p>
-		<p>Coding <Link to={'/coding/'}></Link>.</p>
-        <p>Resume <Link to={'/resume/'}></Link>.</p>
+		<p>Welcome to Thi Nguyen website</p>
+		<p></p>
 	</div>
 );
 export default Home;
