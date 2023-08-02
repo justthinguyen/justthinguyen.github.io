b0ecabe6a16a46d2b362744b9cf1517f5bd509e2
Thi Nguyen <ngthi.nguyen21@gmail.com>
2023-08-01 18:13:41 -0600

    add ColorModeButton component

---
+++ src/components/ColorModeButton.tsx
@@ -0,0 +1,21 @@
+import IconButton from '@mui/material/IconButton';
+import Brightness4Icon from '@mui/icons-material/Brightness4';
+import Brightness7Icon from '@mui/icons-material/Brightness7';
+
+interface BtnProps {
+    mode: string,
+    toggleColorMode: () => void,
+}
+
+export const ColorModeButton = (ps: BtnProps) => {
+    return (
+        <IconButton
+            sx={{
+                ml: 1, bgcolor: 'background.default',
+                color: 'text.primary'
+            }}
+            onClick={ps.toggleColorMode} color="inherit">
+            {ps.mode === 'dark' ? <Brightness7Icon /> : <Brightness4Icon />}
+        </IconButton>
+    );
+}
