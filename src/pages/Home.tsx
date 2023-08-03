16456afef92a9ef62cfa8b068c0be52b9d5e5fe3
Thi Nguyen <ngthi.nguyen21@gmail.com>
2023-08-02 18:53:24 -0600

    Add profile pic in home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,8 +9,20 @@ export default function Home() {
             <Grid item xs={12} md={7} lg={7} sx={{ p: 5 }}>
                 <Intro />
             </Grid>
-            <Grid item xs={12} md={4.5} lg={4.5}>
-                <Box>PICTIURE</Box>
+            <Grid container item xs={12} md={4.5} lg={4.5}>
+                <Box
+                    className="Thi-profile-pic"
+                    component="img"
+                    sx={{
+                        maxHeight: { xs: '90%', md: '100%' },
+                        maxWidth: { xs: '90%', md: '100%' },
+                        marginTop: { xs: '0', md: '5%', lg: '10%' },
+                        borderRadius: '5%'
+                    }}
+                    alt="ThiNguyen-profile-pic"
+                    src="img/ThiNguyen-08-2023.jpeg"
+                >
+                </Box>
 
             </Grid>
 
