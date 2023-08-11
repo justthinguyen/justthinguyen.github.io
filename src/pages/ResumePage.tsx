b5459975756fcb8c9254d38438079ab311ee5064
Thi Nguyen <ngthi.nguyen21@gmail.com>
2023-08-11 14:44:21 -0600

    Display Skills with chips, badges and tooltips - looking good!

--- src/pages/ResumePage.tsx
+++ src/pages/ResumePage.tsx
@@ -1,44 +1,63 @@
 import { Helmet } from 'react-helmet-async';
-import { Box, Grid } from '@mui/material';
+import { Badge, Box, Chip, Grid, Stack } from '@mui/material';
 import { fullName, ThiResume } from '../contents/MyInfo';
 import { ISkills, IWorkHistory, IEducationHistory, IResume } from '../Types';
+import { TextTooltip } from '../components/CustomizedMUI';
 
 const pageName = 'Resume';
 interface ResumeProps {
     contents: IResume
 }
-const Skills = (ps: { skills: ISkills }) => (
-    <>
-        <h3>SKILLS</h3>
-        {Object.keys(ps.skills).map(key =>
-            <div>
-                <h4>{key}</h4>
-                <p>{ps.skills[key]}</p>
-            </div>
-        )}
-    </>
-)
-const WorkHistory = (ps: { workHistory: IWorkHistory[] }) => (
-    <>
-        <h3>WORK HISTORY</h3>
-        {ps.workHistory.map(work =>
-            Object.keys(work).map(key =>
-                <div>
-                    <h4>{key}</h4>
-                    <p>{work[key as keyof IWorkHistory]}</p>
-                </div>
-            )
-        )}
+// TODO: refactor to reuse, responsive layout
+const Skills = (ps: { skills: ISkills }) => {
+    const specialSkillChip = (s: string, y: string) => (
+        <TextTooltip title={y + ' years working experience'} placement="top-end" sx={{ bgcolor: 'transparent' }} >
+            <Badge badgeContent={y} color="primary" sx={{ mr: 1 }}>
+                <Chip label={s} color="secondary" size="small" sx={{ pr: 0.5 }} />
+            </Badge>
+        </TextTooltip>
+    );
+    const skillChip = (s: string) => (<Chip label={s} color="secondary" size="small" />)
 
-    </>
-)
+    const skillsStack = (skills: string[]) => {
+        return (
+            <Stack direction="row" spacing={1} sx={{ pt: 0.5 }}>
+                {skills.map(s => s.split(',')).map((sYear) => (sYear[1] ? specialSkillChip(sYear[0], sYear[1]) : skillChip(sYear[0])))}
+            </Stack>)
+    };
+
+    return (
+        <>
+            <h4>SKILLS</h4>
+            {Object.keys(ps.skills).map(key =>
+                <div><b>{key}</b> {skillsStack(ps.skills[key])}<br /></div>
+            )}
+        </>
+    )
+}
+const WorkHistory = (ps: { workHistory: IWorkHistory[] }) => {
+    return (
+        <>
+            <h3>WORK HISTORY</h3>
+            {ps.workHistory.map(work =>
+                Object.keys(work).map(key =>
+                    <div>
+                        <h4>{key}</h4>
+                        <p>{work[key as keyof IWorkHistory]}</p>
+                    </div>
+                )
+            )}
+
+        </>
+    )
+}
 const Resume = (ps: ResumeProps) => {
     const skills = ps.contents.SKILLS, workHistory = ps.contents['WORK HISTORY'], education = ps.contents.EDUCATION;
     return (
-        <>
+        <Box>
             <Skills skills={skills} />
             <WorkHistory workHistory={workHistory} />
-        </>
+        </Box>
     );
 }
 
