63a1e5be106159387f5f0f4a6fc1e5ae0c9efca4
Thi Nguyen <ngthi.nguyen21@gmail.com>
2023-08-12 17:03:45 -0600

    Add customized Title, Subtitle, Camption MUI components

--- src/components/CustomizedMUI.tsx
+++ src/components/CustomizedMUI.tsx
@@ -1,6 +1,6 @@
 import { styled } from '@mui/material/styles';
 import Tooltip, { TooltipProps, tooltipClasses } from '@mui/material/Tooltip';
-import { Stack } from '@mui/material';
+import { Stack, Typography } from '@mui/material';
 
 export const TextTooltip = styled(({ className, ...props }: TooltipProps) => (
     <Tooltip {...props} classes={{ popper: className }} />
@@ -16,4 +16,22 @@ export const TextTooltip = styled(({ className, ...props }: TooltipProps) => (
 
 export const TextStack = styled(Stack)({
     pt: 0.5, flexWrap: 'wrap', gap: '3px'
+})
+
+export const Title = styled(Typography)({
+    fontWeight: 600,
+    fontSize: '1.25rem',
+    lineHeight: 1.6,
+})
+
+export const Subtitle = styled(Typography)({
+    margin: '6px',
+    fontWeight: 800,
+    fontSize: '1rem'
+})
+
+export const Caption = styled(Typography)({
+    fontWeight: 400,
+    fontSize: '0.9rem',
+    lineHeight: 1.66
 })
