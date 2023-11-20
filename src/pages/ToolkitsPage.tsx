3b2f1ea7c997e8abaf84d556a299020735880465
Thi Nguyen <ngthi.nguyen21@gmail.com>
2023-11-20 12:20:33 -0600

    Add link buttons for Todo app card

--- src/pages/ToolkitsPage.tsx
+++ src/pages/ToolkitsPage.tsx
@@ -1,25 +1,23 @@
 import { Helmet } from 'react-helmet-async';
-import { Grid, Typography, CardMedia, CardActionArea, CardContent, Card } from '@mui/material';
+import { Grid, Typography, CardMedia, CardActions, CardContent, Card } from '@mui/material';
+import { CardLinkButton } from '../components/CustomizedMUI';
 import { getPageTitle } from '../siteInfo';
 
 const TodoAppCard = () => (
     <Card className='ToolkitCard'>
-        <CardActionArea>
-            <CardMedia
-                component="img"
-                height="400px"
-                src="img/Toododo-image.png"
-                alt="Toododo"
-            />
-            <CardContent>
-                <Typography gutterBottom variant="h5" component="div">
-                    Toododo - not your average Todo app.
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                    Add your todo items, drag and drop them to prioritize the items, mark them completed, remove an item or remove all.
-                </Typography>
-            </CardContent>
-        </CardActionArea>
+        <CardMedia component="img" height="400px" src="img/Toododo-image.png" alt="Toododo" />
+        <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+                Toododo - not your average Todo app.
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+                Add your todo items, drag and drop them to prioritize your list, mark them completed, remove an item or remove all to start a new list.
+            </Typography>
+        </CardContent>
+        <CardActions sx={{ justifyContent: 'flex-end' }} >
+            <CardLinkButton href="https://justthinguyen.com/toododo">Use App</CardLinkButton>
+            <CardLinkButton href="https://github.com/ngthing/toododo">Learn More</CardLinkButton>
+        </CardActions>
     </Card>
 );
 
